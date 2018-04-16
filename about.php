8bdc3b9cee9391002ce9e2b5ad192f7aec753915
Alex <alexcotton@mac.com>
2018-04-16 14:59:31 -0700

    spelling

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
   </figure>
   <div class="basic_copy" id="about_copy">
   <!--Very short paragraph about Ruby. The journals should be doing the real work-->
-    <p>This is the story of Ruby Moery, a magnificent, strong, amazing woman who perservered even in the face of terrible tragedy, pain, and loss. She endured many hardships but always kept a smile on her face throughout her 82 years. Those of us left behind are lucky to have a snapshot of her life, as told through her daily diary entries which she diligently recorded in for 40 years. Although her nightly entries are brief, they are like small mosaic tiles building a complete landscape of her life. </p>
+    <p>This is the story of Ruby Moery, a magnificent, strong, amazing woman who persevered even in the face of terrible tragedy, pain, and loss. She endured many hardships but always kept a smile on her face throughout her 82 years. Those of us left behind are lucky to have a snapshot of her life, as told through her daily diary entries which she diligently recorded in for 40 years. Although her nightly entries are brief, they are like small mosaic tiles building a complete landscape of her life. </p>
   </div>
 </main>
 <?php include 'mod/footer.php'; ?>
