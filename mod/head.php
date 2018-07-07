100e43c7744e4a955b45960ebbf3aa3b32fe0096
Alex <alex@alexcotton.com>
2018-07-07 02:14:06 -0700

    favicon trying to fix header and footer image bug... not done yet tho...

--- mod/head.php
+++ mod/head.php
@@ -8,3 +8,12 @@
 
 <link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+
+<!--Favicon stuff-->
+<link rel="apple-touch-icon" sizes="180x180" href="../icon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../icon/favicon-16x16.png">
+<link rel="manifest" href="../icon/site.webmanifest">
+<link rel="mask-icon" href="../icon/safari-pinned-tab.svg" color="#ff0000">
+<meta name="msapplication-TileColor" content="#dc143c">
+<meta name="theme-color" content="#ffffff">
